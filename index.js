551fee95da57ee9d350fdffa535e3ce6b827da25
Nazar Vinnichuk <nazar@vinnich.uk>
2024-03-31 20:13:50 +0300

    Add a simple authentication endpoint.

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+import {
+    ClerkProvider,
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from "@clerk/clerk-react";
 import {Alert} from "@cloudscape-design/components";
+import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 
-const body = <Alert>Hello!</Alert>;
+const key = "pk_test_c3RhYmxlLWNhdGZpc2gtMTMuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
+const body = (
+  <StrictMode>
+    <ClerkProvider publishableKey={key}>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <Alert>Hello!</Alert>
+    </ClerkProvider>
+  </StrictMode>
+);
 
 createRoot(document.getElementById("root")).render(body);
+
+fetch("/api/auth")
+  .then((response) => response.json())
+  .then(console.log);
