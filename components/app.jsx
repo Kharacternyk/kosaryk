7b693eaf96a38b4c94b700d7e6919c79e06f1cec
Nazar Vinnichuk <nazar@vinnich.uk>
2024-04-07 21:00:44 +0300

    Allow deleting bands.

--- components/app.jsx
+++ components/app.jsx
@@ -1,17 +1,22 @@
-import {AppLayout, Button, Input} from "@cloudscape-design/components";
+import {AppLayout, Button, Input, Table} from "@cloudscape-design/components";
 import {Turnstile} from "@marsidev/react-turnstile";
-import {StrictMode, useRef, useState} from "react";
+import {StrictMode, useEffect, useRef, useState} from "react";
 
 export const App = () => {
   const turnstileRef = useRef();
   const [name, setName] = useState("");
+  const [bands, setBands] = useState([]);
 
-  const onGet = async () => {
+  const loadBands = async () => {
     const response = await fetch("/api/bands");
-    console.log(await response.json());
+    setBands(await response.json());
   };
 
-  const onPost = async () => {
+  useEffect(() => {
+    loadBands();
+  }, []);
+
+  const addBand = async () => {
     const url = new URL("/api/bands", document.location);
 
     url.searchParams.append("name", name);
@@ -24,20 +29,60 @@ export const App = () => {
 
     const response = await fetch(url, { method: "post" });
 
-    console.log({ ok: response.ok });
+    if (response.ok) {
+      loadBands();
+    }
   };
 
   const onInput = ({ detail }) => setName(detail.value);
 
+  const deleteBand = async (identity) => {
+    const url = new URL("/api/bands", document.location);
+
+    url.searchParams.append("identity", identity);
+    url.searchParams.append(
+      "token",
+      await turnstileRef.current.getResponsePromise()
+    );
+
+    turnstileRef.current.reset();
+
+    const response = await fetch(url, { method: "delete" });
+
+    if (response.ok) {
+      loadBands();
+    }
+  };
+
+  const columnDefinitions = [
+    {
+      id: "name",
+      header: "Name",
+      cell: (band) => band.name,
+      isRowHeader: true,
+    },
+    {
+      id: "delete",
+      header: "Actions",
+      cell: (band) => (
+        <Button onClick={() => deleteBand(band.identity)}>Delete</Button>
+      ),
+    },
+  ];
+
   const content = (
     <>
+      <Table
+        items={bands}
+        loading={!bands}
+        columnDefinitions={columnDefinitions}
+      ></Table>
       <Turnstile
         ref={turnstileRef}
         siteKey={import.meta.env.viteTurnstileSiteKey}
       />
       <Input value={name} onChange={onInput} />
-      <Button onClick={onPost}>Post</Button>
-      <Button onClick={onGet}>Get</Button>
+      <Button onClick={addBand}>Add</Button>
     </>
   );
 
--- functions/api/bands.js
+++ functions/api/bands.js
@@ -19,9 +19,28 @@ export const onRequestPost = async (context) => {
   return respond();
 };
 
+export const onRequestDelete = async (context) => {
+  const query = getQuery(context);
+  const identity = query.get("identity");
+
+  if (identity == null) {
+    return respond(400);
+  }
+  if (await isBot(query.get("token"), context.env.turnstileSecret)) {
+    return respond(401);
+  }
+
+  await context.env.db
+    .prepare("delete from bands where identity = ?")
+    .bind(identity)
+    .run();
+
+  return respond();
+};
+
 export const onRequestGet = async (context) => {
   const { results } = await context.env.db
-    .prepare("select name from bands")
+    .prepare("select identity, name from bands")
     .all();
 
   return Response.json(results);
